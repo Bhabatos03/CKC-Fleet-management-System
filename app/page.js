a5af9476b9998da3b740f716a15218cefd61879c
Bhabatos03 <bhabatos@ckcsons.com>
2026-09-12 09:31:17 +0530

    Update document text to reflect new system name

--- app/page.js
+++ app/page.js
@@ -1966,7 +1966,7 @@ function Mileage() {
         const pageH = doc.internal.pageSize.getHeight()
         doc.setDrawColor(217, 119, 6); doc.setLineWidth(0.5); doc.line(30, pageH - 32, pageW - 30, pageH - 32)
         doc.setFontSize(8); doc.setTextColor(120); doc.setFont('helvetica', 'normal')
-        doc.text('C. Krishniah Chetty (TM) Group of Jewellers  ·  Fleet Management System  ·  Confidential', 30, pageH - 20)
+        doc.text('C. Krishniah Chetty (TM) Group of Jewellers  ·  FleetPulse  ·  Confidential', 30, pageH - 20)
         doc.text(`Page ${i} of ${pageCount}`, pageW - 30, pageH - 20, { align: 'right' })
       }
 
