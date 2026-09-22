611399ea6181af679a50e586bd5d59aa82b13f0d
Bhabatos03 <bhabatos@ckcsons.com>
2026-09-22 10:22:11 +0530

    Update route.js

--- app/api/[[...path]]/route.js
+++ app/api/[[...path]]/route.js
@@ -3,7 +3,6 @@ import { MongoClient } from 'mongodb'
 import { v4 as uuidv4 } from 'uuid'
 import crypto from 'crypto'
 
-}
 
 const gpVisible = (gp, role, storeId) => {
   if (role === 'admin') return true
